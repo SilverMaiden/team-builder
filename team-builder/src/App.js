c4e85ca637f7d8747805d65487862578a9dbc26f
SilverMaiden <h.specials@gmail.com>
2019-10-16 16:20:14 -0700

    basic working form for team members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,18 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import data from "./data";
 import TeamMembers from './components/TeamMembers';
+import TeamMemberForm from './components/Forms/Forms';
 
 function App() {
   const [teamList, setTeamList] = useState([
     {
-      id: 1,
       name: "tom",
       email: "tom@gmail.com",
       role: "designer"
   }
   ])
+
+  const addNewTeamMember = member => {
+      setTeamList([...teamList, member])
+  }
   return (
     <div className="App">
+        <TeamMemberForm addNewTeamMember={addNewTeamMember} />
         <TeamMembers teamMembersList={teamList} />
      </div>
   );
